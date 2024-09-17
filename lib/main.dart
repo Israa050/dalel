ec312800b6abce3f9030a05671a76cbf32643707
ISRA-ES <israstorytel@gmail.com>
2024-09-17 11:32:18 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Dalel());
+}
+
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text("Dalel App"),
+        ),
+      ),
+    );
+  }
+}
